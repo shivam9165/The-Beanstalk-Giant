3b37b60b0bd0f8e70de8ea4aaf3cf026f92ca53a
shivam sharma <shivam9165sharma@gmail.com>
2020-09-04 17:55:55 +0530

    Commiting after fixing the main menu issue

---
+++ core/src/com/sharmajikapps/thebeanstalkgiant/GameMain.java
@@ -0,0 +1,27 @@
+package com.sharmajikapps.thebeanstalkgiant;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import helpers.GameManager;
+import scenes.MainMenu;
+
+public class GameMain extends Game {
+	private SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		GameManager.getInstance().initializeGameData();
+		setScreen(new MainMenu(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+
+	public SpriteBatch getBatch() {
+		return this.batch;
+	}
+}
